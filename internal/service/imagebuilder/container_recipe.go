ae98cbc827f869c20fe63b553a7eefec0191164e
Alexander <alexander.mancevice@hey.com>
2023-04-07 08:10:13 -0400

    r/aws_imagebuilder_container_recipe: add optional skip_destroy argument

--- internal/service/imagebuilder/container_recipe.go
+++ internal/service/imagebuilder/container_recipe.go
@@ -240,6 +240,11 @@ func ResourceContainerRecipe() *schema.Resource {
 				ForceNew:     true,
 				ValidateFunc: validation.StringInSlice([]string{"Linux", "Windows"}, false),
 			},
+			"skip_destroy": {
+				Type:     schema.TypeBool,
+				Default:  false,
+				Optional: true,
+			},
 			"tags":     tftags.TagsSchema(),
 			"tags_all": tftags.TagsSchemaComputed(),
 			"target_repository": {
@@ -441,6 +446,12 @@ func resourceContainerRecipeUpdate(ctx context.Context, d *schema.ResourceData, 
 
 func resourceContainerRecipeDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
+
+	if v, ok := d.GetOk("skip_destroy"); ok && v.(bool) {
+		log.Printf("[DEBUG] Retaining Imagebuilder Container Recipe version %q", d.Id())
+		return diags
+	}
+
 	conn := meta.(*conns.AWSClient).ImageBuilderConn()
 
 	input := &imagebuilder.DeleteContainerRecipeInput{
